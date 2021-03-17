53af23add9a1acd4f76deb9c3e0bee3203aa71f5
Scoredx <multiusage2@o2.pl>
2021-03-17 15:52:34 +0100

    Final

--- Project1/src/index.ts
+++ Project1/src/index.ts
@@ -1,98 +1,163 @@
 class StatsApp {
-    numberInput: HTMLInputElement;
-    inputsDiv: HTMLDivElement;
-    inputField: HTMLInputElement;
-    resulfDiv: HTMLDivElement;
+
+    //number of input fieds that user wants to generate
+    inputFieldsNumber: number = 0;
+    //array for values from generated fields
+    inputValuesArray: Array<number> = [];
 
     constructor() {
-        this.startApp();
+        const checkInputField: HTMLInputElement = document.querySelector('#inputFieldForGenerator');
+        //event listener for checking the input field
+        checkInputField.addEventListener('input', (event: Event) => 
+        { 
+            const target = event.target as HTMLInputElement;
+            this.inputFieldsNumber = Number(target.value);
+            new ResultUI(this.inputFieldsNumber, this.inputValuesArray);
+        });
+        new ResultUI(this.inputFieldsNumber, this.inputValuesArray);
     }
-
-    startApp(){
-        this.getInputs();
-        this.watchInputValue();
-        this.generateResult();
-    }
-    getInputs(){
-        this.numberInput = document.querySelector('#number');
-        this.inputsDiv = document.querySelector('#inputsDiv');
-        this.resulfDiv = document.querySelector('#resultsDiv');
-
-    }
-    watchInputValue() {
-        this.numberInput.addEventListener('input',() => this.generateInputFields());
-        return;
-    }
-
-    generateInputFields(){
-        let number: number = 1;
-        this.inputsDiv.innerHTML = null;
-        for(let n = 0; n < parseInt(this.numberInput.value); n++){
-            const elementInput = document.createElement('input');
-            elementInput.type = "number";
-            elementInput.id = "input" + n;
-            const elementBr = document.createElement('br');
-            elementBr.id = "br" + n;
-
-            this.inputsDiv.appendChild(elementBr);
-            this.inputsDiv.appendChild(elementInput);
-            number = n;
-        }
-    }
-    generateResult(){
-        let sum: HTMLParagraphElement;
-        let avg: HTMLParagraphElement;
-        let min: HTMLParagraphElement;
-        let max: HTMLParagraphElement;
-
-        sum = document.createElement("p");
-        sum.textContent = "Sum = ";
-        this.resulfDiv.appendChild(sum);
-
-        avg = document.createElement("p");
-        avg.textContent = "Avg = ";
-        this.resulfDiv.appendChild(avg);
-
-        min = document.createElement("p");
-        min.textContent = "Min = ";
-        this.resulfDiv.appendChild(min);
-
-        max = document.createElement("p");
-        max.textContent = "Max = ";
-        this.resulfDiv.appendChild(max);
-    }
-
-        /*
-        let inputList: number[];
-        for(let m = 0; m < this.inputsDiv.childElementCount; m++){
-
-            this.inputField = document.querySelector("#input"+m);
-            let p = parseInt(this.inputField.value);
-            inputList.push(p);
-        }
-        */
-    /*
-    computeData(){
-        const data1 = +this.data1Input.value;
-        const data2 = +this.data2Input.value;
-        const data3 = +this.data3Input.value;
-        const data4 = +this.data4Input.value;
-        const sum = data1 + data2 + data3 + data4;
-        const avg = sum / 4;
-
-        const min = Math.min(data1,data2,data3,data4);
-        const max = Math.max(data1,data2,data3,data4);
-
-        this.showStats(sum,avg,min,max);
-    }
-
-    showStats(sum: number, avg: number, min: number, max: number){
-        this.sumInput.value = sum.toString();
-        this.avgInput.value = avg.toString();
-        this.minInput.value = min.toString();
-        this.maxInput.value = max.toString();
-    }
-    */
 }
 
+class InputFieldContainerGenerator{
+
+    numberInput: HTMLInputElement;
+    removeInputButton: HTMLButtonElement;
+    inputFieldsDiv: HTMLDivElement;
+    inputNumber = document.createElement("b")
+   
+    constructor(inputId, count, inputValuesArray: Array<number>) 
+    {
+
+        // input-field generator
+        this.inputNumber.innerText = inputId + 1 + ".";
+        this.numberInput = document.createElement('input');
+        this.numberInput.type = "number";
+        this.numberInput.id = 'input' + inputId;
+        this.numberInput.value = inputValuesArray[inputId] ? String(inputValuesArray[inputId]) : '0';
+        inputValuesArray[inputId] = Number(this.numberInput.value);
+        this.numberInput.addEventListener('input', (event: Event) => 
+        {
+            const target = event.target as HTMLInputElement;
+            inputValuesArray[inputId] = Number(target.value);
+            new ResultUI(count, inputValuesArray);
+        });
+
+        // delete-button generator
+        this.removeInputButton = document.createElement('button');
+        this.removeInputButton.innerText = "Delete";
+        this.removeInputButton.addEventListener('click', (event: Event) => 
+        {
+            const countInput: HTMLInputElement = document.querySelector('#inputFieldsDiv') as HTMLInputElement;
+            inputValuesArray[inputId] = 0;
+            inputValuesArray.splice(inputId,1);
+            count -= 1;
+            countInput.value = count;
+            new ResultUI(count, inputValuesArray);
+        });
+    }
+    generateInputFields() : HTMLDivElement 
+    {
+        //one container per input-field + delete button
+        const inputFieldContainer = document.createElement('div');
+        inputFieldContainer.className = "inputFieldContainer";
+        inputFieldContainer.appendChild(this.inputNumber);
+        inputFieldContainer.appendChild(this.numberInput);
+        inputFieldContainer.appendChild(this.removeInputButton);
+        return inputFieldContainer;
+    }
+}
+
+class ComputeResult {
+
+    sum: number;
+    avg: number;
+
+    sumResult(inputValues: Array<number>) 
+    {
+        this.sum = inputValues.reduce((a, b) => a + b, 0);
+        return this.sum;
+    }
+    averageResult(inputValues: Array<number>) 
+    {
+        this.avg = inputValues.reduce((a, b) => a + b, 0);
+        return Number((this.avg / inputValues.length).toFixed(3));
+    }
+    minResult(inputValues: Array<number>) 
+    {
+        return Math.min(...inputValues);
+    }
+    maxResult(inputValues: Array<number>) 
+    {
+        return Math.max(...inputValues);
+    }
+
+}
+
+class ResultUI {
+
+    resultsDiv = document.querySelector('#resultsDiv');
+    invalidInputDiv = document.querySelector('#invalidInputDiv')
+    errorMessage = document.createElement('b');
+    inputFieldsDiv = document.getElementById('inputFieldsDiv');
+
+    constructor(inputsAmount: number, inputValues: Array<number>) 
+    {
+        let isValid = false;
+        this.resultsDiv.innerHTML = null;
+
+        if (inputValues && inputsAmount > 0) {
+            isValid = inputValues.every((value) => typeof value === 'number');
+            this.generateUI(inputsAmount, inputValues);
+            this.invalidInputDiv.innerHTML = "";
+        } else {
+            this.invalidInputDiv.innerHTML = "";
+            const inputsSection = document.getElementById('inputFieldsDiv');
+            inputsSection.innerHTML = null;
+            this.errorMessage.innerText = "Write a positive number!";
+            this.invalidInputDiv.appendChild(this.errorMessage);
+        }
+    }
+
+    generateInputs(inputsAmount: number, inputValues: Array<number>): void
+    {        
+        const breakLine = document.createElement('br');
+
+        this.inputFieldsDiv.innerHTML = null;
+        this.inputFieldsDiv.appendChild(breakLine);
+        for (let i = 0; i < inputsAmount; i++) {
+            const input = new InputFieldContainerGenerator(i,inputsAmount, inputValues).generateInputFields();
+            this.inputFieldsDiv.appendChild(input);
+        }
+    }
+    generateUI(inputsAmount: number, inputValues: Array<number>): void 
+    {
+        const stats = new ComputeResult;
+        const resultDivs: Array<HTMLDivElement> = [];
+        this.generateInputs(inputsAmount, inputValues);
+        const valuesFromInputArray = inputValues.slice(0, inputsAmount);
+        // push results into divs 
+        resultDivs.push(this.generateResult('Sum', valuesFromInputArray, stats.sumResult));
+        resultDivs.push(this.generateResult('Avg', valuesFromInputArray, stats.averageResult));
+        resultDivs.push(this.generateResult('Min', valuesFromInputArray, stats.minResult));
+        resultDivs.push(this.generateResult('Max', valuesFromInputArray, stats.maxResult));
+        resultDivs.forEach((element: HTMLDivElement) => 
+        {
+            this.resultsDiv.appendChild(element);
+        });
+    }
+    generateResult(name: string, inputValues: Array<number>, ComputeResult: Function): HTMLDivElement 
+    {
+        const resultName = document.createElement('p');
+        const value = document.createElement('b');
+        const container = document.createElement('div');
+
+        resultName.innerText = name;
+        value.innerText = ComputeResult(inputValues);
+        container.style.float = "left";
+        container.style.marginLeft = "20px"
+        container.appendChild(resultName);
+        container.appendChild(value);
+        return container;
+    }
+}
 const statsApp = new StatsApp();
