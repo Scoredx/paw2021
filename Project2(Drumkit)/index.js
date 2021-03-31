b74c8f01262fcd48eaa304a2d488626f3dc5e22c
Scoredx <multiusage2@o2.pl>
2021-03-31 23:08:56 +0200

    recordig, checkbox handling, playing single tracks

--- Project2(Drumkit)/index.js
+++ Project2(Drumkit)/index.js
@@ -1,45 +1,173 @@
+var soundElements = [document.querySelector('[data-sound="boom"]'),
+    document.querySelector('[data-sound="clap"]'),
+    document.querySelector('[data-sound="hihat"]'),
+    document.querySelector('[data-sound="kick"]'),
+    document.querySelector('[data-sound="openhat"]'),
+    document.querySelector('[data-sound="ride"]'),
+    document.querySelector('[data-sound="snare"]'),
+    document.querySelector('[data-sound="tink"]')
+];
+var Sounds;
+(function (Sounds) {
+    Sounds[Sounds["boom"] = 0] = "boom";
+    Sounds[Sounds["clap"] = 1] = "clap";
+    Sounds[Sounds["hithat"] = 2] = "hithat";
+    Sounds[Sounds["kick"] = 3] = "kick";
+    Sounds[Sounds["openhat"] = 4] = "openhat";
+    Sounds[Sounds["ride"] = 5] = "ride";
+    Sounds[Sounds["snare"] = 6] = "snare";
+    Sounds[Sounds["tink"] = 7] = "tink";
+})(Sounds || (Sounds = {}));
+var playButtonsArray = [document.querySelector('#track1Play'),
+    document.querySelector('#track2Play'),
+    document.querySelector('#track3Play'),
+    document.querySelector('#track4Play')
+];
+var recordButtonsArray = [document.querySelector('#track1Record'),
+    document.querySelector('#track2Record'),
+    document.querySelector('#track3Record'),
+    document.querySelector('#track4Record')];
+var checkBoxesArray = [document.querySelector('#track1Checkbox'),
+    document.querySelector('#track2Checkbox'),
+    document.querySelector('#track3Checkbox'),
+    document.querySelector('#track4Checkbox')];
 document.body.addEventListener('keypress', onKeyPress);
+recordButtonsArray[0].addEventListener('click', recordChannel1);
+recordButtonsArray[1].addEventListener('click', recordChannel2);
+recordButtonsArray[2].addEventListener('click', recordChannel3);
+recordButtonsArray[3].addEventListener('click', recordChannel4);
+playButtonsArray[0].addEventListener('click', playChannel1);
+playButtonsArray[1].addEventListener('click', playChannel2);
+playButtonsArray[2].addEventListener('click', playChannel3);
+playButtonsArray[3].addEventListener('click', playChannel4);
+var channel1 = [];
+var channel2 = [];
+var channel3 = [];
+var channel4 = [];
+var startRecordTime1 = 0;
+var startRecordTime2 = 0;
+var startRecordTime3 = 0;
+var startRecordTime4 = 0;
+function trackCheckboxCheck(checkbox) {
+    if (checkbox.checked === true) {
+        return true;
+    }
+    if (checkbox.checked === false) {
+        return false;
+    }
+}
 function onKeyPress(ev) {
-    var boom = document.querySelector('[data-sound="boom"]');
-    var clap = document.querySelector('[data-sound="clap"]');
-    var hihat = document.querySelector('[data-sound="hihat"]');
-    var kick = document.querySelector('[data-sound="kick"]');
-    var openhat = document.querySelector('[data-sound="openhat"]');
-    var ride = document.querySelector('[data-sound="ride"]');
-    var snare = document.querySelector('[data-sound="snare"]');
-    var tink = document.querySelector('[data-sound="tink"]');
-    switch (ev.key) {
+    var key = ev.key;
+    var time = ev.timeStamp;
+    if (trackCheckboxCheck(checkBoxesArray[0])) {
+        channel1.push({
+            key: key,
+            time: time
+        });
+    }
+    if (trackCheckboxCheck(checkBoxesArray[1])) {
+        channel2.push({
+            key: key,
+            time: time
+        });
+    }
+    if (trackCheckboxCheck(checkBoxesArray[2])) {
+        channel3.push({
+            key: key,
+            time: time
+        });
+    }
+    if (trackCheckboxCheck(checkBoxesArray[3])) {
+        channel4.push({
+            key: key,
+            time: time
+        });
+    }
+    playSound(key);
+    console.log(channel1);
+    //console.log(channel2);
+    //console.log(channel3);
+    //console.log(channel4);
+}
+function recordChannel1(ev) {
+    channel1 = [];
+    startRecordTime1 = ev.timeStamp;
+    console.log(ev.timeStamp);
+}
+function recordChannel2(ev) {
+    channel1 = [];
+    startRecordTime2 = ev.timeStamp;
+    console.log(ev.timeStamp);
+}
+function recordChannel3(ev) {
+    channel1 = [];
+    startRecordTime3 = ev.timeStamp;
+    console.log(ev.timeStamp);
+}
+function recordChannel4(ev) {
+    channel1 = [];
+    startRecordTime4 = ev.timeStamp;
+    console.log(ev.timeStamp);
+}
+function playSound(key) {
+    switch (key) {
         case 'q':
-            //boom.currentTime = 0;
-            boom.play();
+            soundElements[Sounds.boom].currentTime = 0;
+            soundElements[Sounds.boom].play();
             break;
         case 'w':
-            //clap.currentTime = 0;
-            clap.play();
+            soundElements[Sounds.clap].currentTime = 0;
+            soundElements[Sounds.clap].play();
             break;
         case 'e':
-            //hihat.currentTime = 0;
-            hihat.play();
+            soundElements[Sounds.hithat].currentTime = 0;
+            soundElements[Sounds.hithat].play();
             break;
         case 'r':
-            //kick.currentTime = 0;
-            kick.play();
+            soundElements[Sounds.kick].currentTime = 0;
+            soundElements[Sounds.kick].play();
             break;
+        ////////////////////////////////
         case 'a':
-            //openhat.currentTime = 0;
-            openhat.play();
+            soundElements[Sounds.openhat].currentTime = 0;
+            soundElements[Sounds.openhat].play();
             break;
         case 's':
-            //ride.currentTime = 0;
-            ride.play();
+            soundElements[Sounds.ride].currentTime = 0;
+            soundElements[Sounds.ride].play();
             break;
         case 'd':
-            //snare.currentTime = 0;
-            snare.play();
+            soundElements[Sounds.snare].currentTime = 0;
+            soundElements[Sounds.snare].play();
             break;
         case 'f':
-            //  tink.currentTime = 0;
-            tink.play();
+            soundElements[Sounds.tink].currentTime = 0;
+            soundElements[Sounds.tink].play();
             break;
     }
+}
+function playChannel1() {
+    channel1.forEach(function (sound) {
+        console.log(startRecordTime1 - sound.time);
+        var timeout = sound.time - startRecordTime1;
+        setTimeout(function () { return playSound(sound.key); }, timeout);
+    });
+}
+function playChannel2() {
+    channel2.forEach(function (sound) {
+        var timeout = sound.time - startRecordTime2;
+        setTimeout(function () { return playSound(sound.key); }, timeout);
+    });
+}
+function playChannel3() {
+    channel3.forEach(function (sound) {
+        var timeout = sound.time - startRecordTime3;
+        setTimeout(function () { return playSound(sound.key); }, timeout);
+    });
+}
+function playChannel4() {
+    channel4.forEach(function (sound) {
+        var timeout = sound.time - startRecordTime4;
+        setTimeout(function () { return playSound(sound.key); }, timeout);
+    });
 }
--- Project2(Drumkit)/index.ts
+++ Project2(Drumkit)/index.ts
@@ -1,58 +1,199 @@
+const soundElements: HTMLAudioElement[] = [document.querySelector('[data-sound="boom"]'), 
+                                        document.querySelector('[data-sound="clap"]'),
+                                        document.querySelector('[data-sound="hihat"]'),
+                                        document.querySelector('[data-sound="kick"]'),
+                                        document.querySelector('[data-sound="openhat"]'),
+                                        document.querySelector('[data-sound="ride"]'),
+                                        document.querySelector('[data-sound="snare"]'),
+                                        document.querySelector('[data-sound="tink"]') 
+                                    ];
+enum Sounds {
+    boom = 0,
+    clap = 1,
+    hithat = 2,
+    kick = 3,
+    openhat = 4,
+    ride = 5,
+    snare = 6,
+    tink = 7
+}
+const playButtonsArray: HTMLButtonElement[] = [document.querySelector('#track1Play'),
+                                          document.querySelector('#track2Play'),
+                                          document.querySelector('#track3Play'),
+                                          document.querySelector('#track4Play')
+                                        ];
+const recordButtonsArray: HTMLButtonElement[] = [document.querySelector('#track1Record'),
+                                            document.querySelector('#track2Record'),
+                                            document.querySelector('#track3Record'),
+                                            document.querySelector('#track4Record')];
+
+const checkBoxesArray: HTMLInputElement[] = [document.querySelector('#track1Checkbox'),
+                                            document.querySelector('#track2Checkbox'),
+                                            document.querySelector('#track3Checkbox'),
+                                            document.querySelector('#track4Checkbox')];
+
 document.body.addEventListener('keypress', onKeyPress);
+
+recordButtonsArray[0].addEventListener('click', recordChannel1);
+recordButtonsArray[1].addEventListener('click', recordChannel2);
+recordButtonsArray[2].addEventListener('click', recordChannel3);
+recordButtonsArray[3].addEventListener('click', recordChannel4);
+
+playButtonsArray[0].addEventListener('click', playChannel1);
+playButtonsArray[1].addEventListener('click', playChannel2);
+playButtonsArray[2].addEventListener('click', playChannel3);
+playButtonsArray[3].addEventListener('click', playChannel4);
+
+let channel1: any[] = [];
+let channel2: any[] = [];
+let channel3: any[] = [];
+let channel4: any[] = [];
+
+let startRecordTime1: number = 0;
+let startRecordTime2: number = 0;
+let startRecordTime3: number = 0;
+let startRecordTime4: number = 0;
+
+function  trackCheckboxCheck(checkbox: HTMLInputElement): boolean{
+    if(checkbox.checked === true){
+        return true;
+    }
+    if(checkbox.checked === false){
+        return false;
+    }
+}
 
 function onKeyPress(ev: KeyboardEvent): void {
 
-    const boom: HTMLAudioElement = document.querySelector('[data-sound="boom"]');
-    const clap: HTMLAudioElement = document.querySelector('[data-sound="clap"]');
-    const hihat: HTMLAudioElement = document.querySelector('[data-sound="hihat"]');
-    const kick: HTMLAudioElement = document.querySelector('[data-sound="kick"]');
+    const key = ev.key;
+    const time = ev.timeStamp;
 
-    const openhat: HTMLAudioElement = document.querySelector('[data-sound="openhat"]');
-    const ride: HTMLAudioElement = document.querySelector('[data-sound="ride"]');
-    const snare: HTMLAudioElement = document.querySelector('[data-sound="snare"]');
-    const tink: HTMLAudioElement = document.querySelector('[data-sound="tink"]');
+    if(trackCheckboxCheck(checkBoxesArray[0])){
+        channel1.push({
+            key,
+            time
+        })
+    }
+    if(trackCheckboxCheck(checkBoxesArray[1])){
+        channel2.push({
+            key,
+            time
+        })
+    }
+    if(trackCheckboxCheck(checkBoxesArray[2])){
+        channel3.push({
+            key,
+            time
+        })
+    }
+    if(trackCheckboxCheck(checkBoxesArray[3])){
+        channel4.push({
+            key,
+            time
+        })
+    }
+    playSound(key);
+    console.log(channel1);
+    //console.log(channel2);
+    //console.log(channel3);
+    //console.log(channel4);
+}   
 
-    switch(ev.key){
+function recordChannel1(ev: Event): void{
+    channel1 = [];
+    startRecordTime1 = ev.timeStamp;
+    console.log(ev.timeStamp);
+}
+function recordChannel2(ev: Event): void{
+    channel1 = [];
+    startRecordTime2 = ev.timeStamp;
+    console.log(ev.timeStamp);
+}
+function recordChannel3(ev: Event): void{
+    channel1 = [];
+    startRecordTime3 = ev.timeStamp;
+    console.log(ev.timeStamp);
+}
+function recordChannel4(ev: Event): void{
+    channel1 = [];
+    startRecordTime4 = ev.timeStamp;
+    console.log(ev.timeStamp);
+}
+function playSound(key: string){
+
+    switch(key){
         case 'q':
-            //boom.currentTime = 0;
-            boom.play()
+            soundElements[Sounds.boom].currentTime = 0;
+            soundElements[Sounds.boom].play()
         break;
 
         case 'w':
-            //clap.currentTime = 0;
-            clap.play()
+            soundElements[Sounds.clap].currentTime = 0;
+            soundElements[Sounds.clap].play()
         break;
 
         case 'e':
-            //hihat.currentTime = 0;
-            hihat.play()
+            soundElements[Sounds.hithat].currentTime = 0;
+            soundElements[Sounds.hithat].play()
         break;
 
         case 'r':
-            //kick.currentTime = 0;
-            kick.play()
+            soundElements[Sounds.kick].currentTime = 0;
+            soundElements[Sounds.kick].play()
         break;
-
-        
-
+        ////////////////////////////////
         case 'a':
-            //openhat.currentTime = 0;
-            openhat.play()
+            soundElements[Sounds.openhat].currentTime = 0;
+            soundElements[Sounds.openhat].play()
         break;
 
         case 's':
-            //ride.currentTime = 0;
-            ride.play()
+            soundElements[Sounds.ride].currentTime = 0;
+            soundElements[Sounds.ride].play()
         break;
 
         case 'd':
-            //snare.currentTime = 0;
-            snare.play()
+            soundElements[Sounds.snare].currentTime = 0;
+            soundElements[Sounds.snare].play()
         break;
 
         case 'f':
-            //  tink.currentTime = 0;
-            tink.play()
+            soundElements[Sounds.tink].currentTime = 0;
+            soundElements[Sounds.tink].play()
         break;
     }
-}   
+}
+
+function playChannel1(): void {
+
+    channel1.forEach(sound =>{
+        console.log(startRecordTime1 - sound.time);
+        const timeout = sound.time - startRecordTime1;
+        setTimeout(() => playSound(sound.key),timeout);
+    })
+}
+function playChannel2(): void {
+
+    channel2.forEach(sound =>{
+        const timeout = sound.time - startRecordTime2;
+        setTimeout(() => playSound(sound.key),timeout);
+    })
+}
+
+function playChannel3(): void {
+
+    channel3.forEach(sound =>{
+        const timeout = sound.time - startRecordTime3;
+        setTimeout(() => playSound(sound.key),timeout);
+    })
+}
+
+function playChannel4(): void {
+
+    channel4.forEach(sound =>{
+        const timeout = sound.time - startRecordTime4;
+        setTimeout(() => playSound(sound.key),timeout);
+    })
+}
+
+
