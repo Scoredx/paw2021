fadbfd093c370ae1bd73f10b0c15e13c6fc3ce00
Scoredx <multiusage2@o2.pl>
2021-05-19 23:09:06 +0200

    refactored base functions from 'Project3'

--- Project4(NoteKeep)/src/app.ts
+++ Project4(NoteKeep)/src/app.ts
@@ -1,97 +1,101 @@
-import { IWeatherData, IWeather } from './interface';
-
 export class App {
-    opwApiKey = 'cee0399a2098bebcbc13c9ef29cbbf7d';
-    cityArray: string[] = [];
+    titleArr: string[] = [];
+    textArr: string[] = [];
     counter: number = 0;
 
     constructor() {
         
     }
-    // saveData(data: any) {
-    //     localStorage.setItem('weatherData', JSON.stringify(data));
-    // }
-    
-    // getData() {
-    //     const data = localStorage.getItem('weatherData');
-    //     if (data) {
-    //         return JSON.parse(data);
-    //     } 
-    //     else 
-    //     {
-    //         return {};
-    //     }
-    // }
-    
-    // saveCitiesToLocalStorage(cities: string[]){
-    //     if(cities == []){
-    //         return;
-    //     }else{
-    //         let cityData: string[] = this.getCities();
-    //         if(cityData){
-    //             cityData.forEach(element => {
-    //                 cities.push(element);
-    //             });
-    //         }
-    //         localStorage.setItem('cityData', JSON.stringify(cities));
-    //     }
-    // }
+    getTitle(titleString: string){
+        this.titleArr.push(titleString);
+        console.log(this.titleArr);
+    }
+    getText(textString: string){
+        this.textArr.push(textString)
+        console.log(this.textArr);
+    }
 
-    // getCities(){
-    //     const cityData = localStorage.getItem('cityData');
-    //     if (cityData) {
-    //         return JSON.parse(cityData);
-    //     } 
-    //     else 
-    //     {
-    //         return;
-    //     }
-    // }
+    saveData(data:any){
+        localStorage.setItem('weatherData', JSON.stringify(data));
+    }
 
-    // saveCityArray(str: string){
-        
-    //     if(this.cityArray.includes(str)){
-    //         return;
-    //     }else{
-    //         this.cityArray.push(str);
-    //     }
-    // }
 
-    // getCityName(){
-    //     var str = (<HTMLInputElement>document.getElementById("inputField")).value;
-    //     return str;
-    // }
+    saveTitleAndText(titles: string[], text: string[]){
+        localStorage.setItem('titles', JSON.stringify(titles));
+        localStorage.setItem('text', JSON.stringify(text));
+    }
+
+    getTitlesFromLS(){
+        const titles = localStorage.getItem('titles');
+        if (titles) {
+            return JSON.parse(titles);
+        } 
+        else 
+        {
+            return;
+        }
+    }
+
+    getTextFromLS(){
+        const text = localStorage.getItem('text');
+        if (text) {
+            return JSON.parse(text);
+        } 
+        else 
+        {
+            return;
+        }
+    }
 
     createNote(noteTitle: string, noteText: string){
         let note: HTMLDivElement = document.createElement("div");
         note.id = "note" + this.counter;
         note.className = "note";
 
+        let noteInnerWrapper: HTMLDivElement = document.createElement("div");
+        noteInnerWrapper.id = "noteInnerWrapper" + this.counter;
+        noteInnerWrapper.className = "noteInnerWrapper";
+        
+
         let noteDragDiv: HTMLDivElement = document.createElement("div");
         noteDragDiv.id = "noteDrag" + this.counter;
         noteDragDiv.className = "noteDrag";
+        
+        let title: HTMLSpanElement =  document.createElement("span");
+        title.id = "noteTitle" + this.counter;
+        title.className = "noteTitle";
+        title.innerHTML = noteTitle;
 
         let noteTitleDiv: HTMLDivElement = document.createElement("div");
         noteTitleDiv.id = "noteTitle" + this.counter;
         noteTitleDiv.className = "noteTitle";
-        noteTitleDiv.innerHTML = noteTitle;
 
         let noteCloseButton: HTMLButtonElement = document.createElement("button");
         noteCloseButton.id = "noteCloseButton" + this.counter;
         noteCloseButton.className = "noteCloseButton";
+        noteCloseButton.innerHTML = 'X';
 
-        let noteTextDiv: HTMLDivElement = document.createElement("div");
-        noteTextDiv.id = "noteText" + this.counter;
-        noteTextDiv.className = "noteText";
-        noteTextDiv.innerHTML = noteText;
+        noteCloseButton.onclick = function(){
+            noteCloseButton.parentNode.parentNode.parentNode.removeChild(noteCloseButton.parentNode.parentNode);
+        }
+
+
+        let noteTextArea: HTMLTextAreaElement = document.createElement("textarea");
+        noteTextArea.id = "noteTextArea" + this.counter;
+        noteTextArea.className = "noteTextArea";
+        noteTextArea.innerHTML = noteText;
+        noteTextArea.rows =8;
 
         note.appendChild(noteDragDiv);
-        note.appendChild(noteTitleDiv);
-        note.appendChild(noteCloseButton);
-        note.appendChild(noteTextDiv);
+        note.appendChild(noteInnerWrapper);
+
+        noteInnerWrapper.appendChild(noteTitleDiv);
+        noteTitleDiv.appendChild(title);
+        noteInnerWrapper.appendChild(noteCloseButton);
+        noteInnerWrapper.appendChild(noteTextArea);
+        
 
         this.counter++;
         return note;
     }
 }
-  
--- Project4(NoteKeep)/src/index.ts
+++ Project4(NoteKeep)/src/index.ts
@@ -1,41 +1,37 @@
 import { App } from './app';
 import './main.scss';
 
-const createNote: HTMLDivElement = document.querySelector("#city");
 const resultDiv: HTMLDivElement = document.querySelector("#result");
+const inputTitle: HTMLInputElement = document.querySelector("#inputTitle");
+const inputText: HTMLInputElement = document.querySelector("#inputText");
+const submitButton: HTMLButtonElement = document.querySelector("#submitButton");
+const noteCloseButton: HTMLButtonElement = document.querySelector(".noteCloseButton");
 
-let noteTitle: HTMLInputElement = document.createElement("input");
-noteTitle.id ="inputField";
-noteTitle.innerText="";
-
-let noteText: HTMLInputElement = document.createElement("input");
-noteText.id ="inputField";
-noteText.innerText="";
-
-let submitButton: HTMLButtonElement = document.createElement("button");
-submitButton.id = "submitButton";
-submitButton.innerHTML = "Add";
-
-createNote.appendChild(noteTitle);
-createNote.appendChild(noteText);
-createNote.appendChild(submitButton);
 
 submitButton.addEventListener('click', () => {
 
-    resultDiv.appendChild(app.createNote(noteTitle.value, noteText.value));
+    resultDiv.appendChild(app.createNote(inputTitle.value, inputText.value));
+    app.getTitle(inputTitle.value);
+    app.getText(inputText.value);
+
 });
 
-// window.addEventListener('beforeunload', function() {
-//     //app.saveCitiesToLocalStorage(app.cityArray);
-// });
 
-// window.addEventListener('load', () => {
-//     // let cities: string[] = app.getCities();
-//     // if(cities){
-//     //     cities.forEach((city, countCity) => {
-            
-//     //     });
-//     // }
-// });
+window.addEventListener('beforeunload', function() {
+    app.saveTitleAndText(app.titleArr, app.textArr);
+});
+
+window.addEventListener('load', () => {
+    let titlesLS: string[] = app.getTitlesFromLS();
+    let textLS: string[] = app.getTextFromLS();
+
+    if(titlesLS && textLS){
+        titlesLS.forEach((x, index) => {
+            app.titleArr[index] = titlesLS[index];
+            app.textArr[index] = textLS[index];
+            resultDiv.appendChild(app.createNote(titlesLS[index],textLS[index]));
+        });
+    }
+});
 
 const app = new App();
