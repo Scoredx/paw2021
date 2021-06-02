f626a7e24eba6f56433fbc48166684c84b3c96c5
Scoredx <multiusage2@o2.pl>
2021-06-02 23:38:01 +0200

    + working firebase CRUD functions

--- Project4(NoteKeep)LocalStorage/src/index.ts
+++ Project4(NoteKeep)LocalStorage/src/index.ts
@@ -4,49 +4,6 @@ import Note from './Note';
 import Notes from './notes';
 import './SCSS/reset.scss';
 import './SCSS/main.scss';
-import firebase from 'firebase';
-import { firebaseConfig } from './config';
-
-
-const firebaseApp = firebase.initializeApp(firebaseConfig);
-const db = firebaseApp.firestore();
-
-const notee = {
-    title: "Second Note",
-    text: "Note Note",
-    //date: "3-3-2020",
-    isPinned: false,
-};
-
-addNote(notee);
-async function addNote(notee: any){
-    const res = await db.collection('notes').add(notee);
-}
- 
-deleteNote('I3w666V5VbQJY1BzpeQu');
-async function deleteNote(id: string){
-    const res = await db.collection('notes').doc(id).delete();
-}
-
-updateNote('WvQbk3Ok1PE7EdQF24ia',
-    {
-        title: 'asdasd',
-        text: 'asd'
-    }
-);
-async function updateNote(id: string, note: any){
-    const res = await db.collection('notes').doc(id).update(note);
-}
-
-getNote('icYPPdH7R06pKBYBuFPo').then(res => console.log(res));
-async function getNote(id: string){
-    return db.collection('notes').doc(id).get().then(res => ({id: res.id, data: res.data()}));
-}
-
-getNotes().then(res => console.log(res));
-async function getNotes(){
-    return db.collection('notes').get().then(res => ({size: res.size, docs: res.docs}));
-}
 
 const note = new Note();
 const notes = new Notes();
@@ -58,9 +15,7 @@ const submitButton: HTMLButtonElement = document.querySelector("#submitButton");
 notes.pinnedDiv = pinned;
 notes.notesDiv = notesDiv;
 
-
 submitButton.addEventListener('click', () => {
-    
     let newNote = note.saveToNote(app.counter, inputTitle.value,inputText.value, "lightgray", false);
     note.noteToArr(newNote);
     notes.notesDiv.appendChild(notes.createNote(newNote));
@@ -71,7 +26,6 @@ window.addEventListener('beforeunload', function() {
 });
 
 window.addEventListener('load', () => {
-    
     app.noteLS = appStorage.getNotesFromLocalStorage();
     
     if(app.noteLS){
@@ -87,6 +41,3 @@ window.addEventListener('load', () => {
         });
     }
 });
-
-
-
